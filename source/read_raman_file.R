c61dd07c7858d6f77a24d84110363a89fa1481e4
tacaro <tristan.caro@colorado.edu>
2022-08-10 14:46:32 -0600

    function to read Raman data

---
+++ source/read_raman_file.R
@@ -0,0 +1,26 @@
+#' Read in Raman spectrum
+#' @param file character vector: directory of Raman file
+#' @return tibble containing Raman intensity
+#' 
+
+read_raman_file <- function(file) {
+  df <- readr::read_delim(
+    file,
+    col_names = FALSE
+    ) %>% 
+    rename(
+      `wavenumber_cm` = X1,
+      `intensity` = X2
+    ) %>% 
+    # Add file info
+    mutate(
+      filename = file
+    ) %>% 
+    # Add band limits
+    mutate(
+      band = case_when(
+        between(wavenumber_cm, 2040, 2300) ~ "CD",
+        between(wavenumber_cm, 2800, 3100) ~ "CH"
+      )
+    )
+}
