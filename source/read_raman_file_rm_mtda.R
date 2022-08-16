cd5dc384a2a65a6ed3b40ebaf247e159c67ea176
tacaro <tristan.caro@colorado.edu>
2022-08-16 16:22:04 -0600

    read in raman spectrum that has metadata

---
+++ source/read_raman_file_rm_mtda.R
@@ -0,0 +1,29 @@
+#' Read in Raman spectrum
+#' @param file character vector: directory of Raman file
+#' @return tibble containing Raman intensity
+#' 
+
+read_raman_file_rm_mtda <- function(file) {
+  df <- readr::read_delim(
+    file,
+    col_names = FALSE,
+    col_types = cols(),
+    comment = "#",
+    delim = '\t'
+    ) %>% 
+    rename(
+      `wavenumber_cm` = X1,
+      `intensity` = X2
+    ) %>% 
+    # Add file info
+    mutate(
+      filename = file
+    ) %>% 
+    # Add band limits
+    mutate(
+      band = case_when(
+        between(wavenumber_cm, 2040, 2300) ~ "CD",
+        between(wavenumber_cm, 2800, 3100) ~ "CH"
+      )
+    )
+}
